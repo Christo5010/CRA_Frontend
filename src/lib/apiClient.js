a75242dd61d2f4ae03d7029a5184b1aa0d805ab3
Tayyab Aslam <thisistayyab@outlook.com>
2025-09-10 23:57:17 +0500

    Backend Api Changed

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://cra-backend.vercel.app/v1/api';
+// const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://cra-backend.vercel.app/v1/api';
+
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://cra-backend-iota.vercel.app/v1/api';
 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/v1/api';
 
